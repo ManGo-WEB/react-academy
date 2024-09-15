492724941003b4f1da9ee28cad87ba261ead9aca
Nikita Vampilov <info@mango-web.ru>
2024-09-15 14:06:24 +1000

    Add homework

--- 01/homework.js
+++ 01/homework.js
@@ -38,3 +38,34 @@ function makeBusiness(owner, director, cash, emp) {
     console.log('Note. Be careful with ' + sponsors.eu[0] + ". It's a huge risk.");
 }
 makeBusiness.apply(null, ['Sam', null, money, employersNames]);
+
+
+// HOMEWORK 
+const employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+
+let employersNames = employers.filter(employer => employer.trim().length > 0)
+                             .map(employer => employer.toLowerCase().trim());
+
+const sponsors = {
+  cash: [40000, 5000, 30400, 12000],
+  eu: ['SRL', 'PLO', 'J&K'],
+  rus: ['RusAuto', 'SBO']
+};
+
+const calcCash = (own = 0, ...everyCash) => everyCash.reduce((total, cash) => total + cash, own);
+
+const money = calcCash(null, ...sponsors.cash);
+
+const makeBusiness = (owner, director = 'Victor', cash, emp) => {
+	if (director === null || director === undefined) {
+    director = 'Victor';
+  }
+  const sumSponsors = [...sponsors.eu, ...sponsors.rus, 'unexpected sponsor'];
+  
+  console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}`);
+  console.log('And we have a sponsors: ');
+  console.log(...sumSponsors);
+  console.log(`Note. Be careful with ${sponsors.eu[0]}. It's a huge risk.`);
+};
+
+makeBusiness('Sam', null, money, employersNames);
